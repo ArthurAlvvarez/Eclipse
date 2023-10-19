22a55077be4881bdc810491f79d42b9f411aefa5
Arturo <arturo.alvarez.gomez@colegiolitterator.com>
2023-10-19 19:26:29 +0200

    cambio variable

--- while/src/ejercicios/ejercicio4.java
+++ while/src/ejercicios/ejercicio4.java
@@ -7,24 +7,24 @@ public class ejercicio4 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int numero = (int)(Math.random()*100+1);
+		int random = (int)(Math.random()*100+1);
 		
 		System.out.println("Dime un número entre 1 y 100: ");
 		
-		int teclado = sc.nextInt();
+		int numero = sc.nextInt();
 		
-		while ( teclado!=numero ){
+		while ( numero != random ){
 		
-		if(teclado > numero) {
+		if(numero > random) {
 			
 			System.out.println("Baja baja");
 		}
-		else if(teclado< numero){
+		else if(numero < random){
 			
 			System.out.println("Sube sube");
 		}
 		
-		  teclado = sc.nextInt();
+		  numero = sc.nextInt();
 		}
 		  
 		  System.out.println("Bien chaval bien");
